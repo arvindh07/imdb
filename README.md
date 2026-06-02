ae3daba110d90c1ef0986a21ea067a4fc6ab2242
arvindh <arvindhganesh11@gmail.com>
2026-06-02 11:24:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IMDB React App
+
+- This is a simple project to brush up react knowledge
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Imdb</h1>
+    </>
+  )
+}
+
+export default App
