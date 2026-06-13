b7d76369341da626d216aa58d50e1db6d05b5fca
arvindh <arvindhganesh11@gmail.com>
2026-06-13 10:36:38 +0530

    add icons, axios and header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Header = () => {
     return (
         <div className="flex px-12 py-3 border-b border-yellow-400 items-center justify-between">
-            <h2 className="text-2xl bg-yellow-300 px-4 py-2 rounded-xl font-extrabold tracking-widest">IMDb</h2>
+            <h2 className="text-2xl bg-yellow-300 px-4 py-2 rounded-xl font-extrabold tracking-widest cursor-pointer">
+                <Link to="/">IMDb</Link>
+            </h2>
             <div className="flex space-x-4 items-center font-sans">
                 <NavLink to="/" className={({ isActive }) =>
                     isActive
