785be13de40850cd547c99925b8786ace513e66f
arvindh <arvindhganesh11@gmail.com>
2026-06-13 22:00:45 +0530

    empty pagination and add watchlist

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
 import axiosInstance from "./axios"
 
-export const fetchData = async (url) => {
+export const fetchData = async (url, query) => {
     let response = {
         data: null,
         error: null
     }
     try {
-        const res = await axiosInstance.get(url + `?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
+        const localUrl = query ? url + `?api_key=${import.meta.env.VITE_TMDB_API_KEY}` + query : url + `?api_key=${import.meta.env.VITE_TMDB_API_KEY}`;
+        const res = await axiosInstance.get(localUrl);
         if (!res.data) {
             throw new Error("No data found")
         }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react"
 import { fetchData } from "../api/api"
 import { urls } from "../api/urls"
 import { getGenreName, getImageURL } from "../utils/common";
+import { Plus } from "lucide-react";
 
 const MovieList = () => {
     const [trendingMovies, setTrendingMovies] = useState([]);
     const [genres, setGenres] = useState([]);
+    const [page, setPage] = useState(2);
+    const [watchList, setWatchList] = useState([]);
 
     const fetchTrendingMovies = async () => {
-        const response = await fetchData(urls.TRENDING_MOVIES);
+        const response = await fetchData(urls.TRENDING_MOVIES, `&page=${page}`);
 
         if (!response.error) {
             setTrendingMovies(response.data?.results || []);
+            setTotalPages(response.data?.total_pages || 1);
         }
     }
 
@@ -21,6 +25,19 @@ const MovieList = () => {
         if (!response.error) {
             setGenres(response.data?.genres || []);
         }
+    }
+
+    const handleAddToWatchList = (movie) => {
+        setWatchList((prev) => [...prev, movie]);
+    }
+
+    const handleRemoveToWatchList = (movieId) => {
+        const newList = watchList.filter((movie) => movie.id !== movieId);
+        setWatchList(newList);
+    }
+
+    const checkIfPresentInWatchlist = (movieId) => {
+        return Boolean(watchList.find((movie) => movie.id === movieId));
     }
 
     useEffect(() => {
@@ -33,10 +50,11 @@ const MovieList = () => {
             <h2 className="mb-6 ml-30 text-2xl font-bold underline decoration-amber-500 underline-offset-8">Trending Movies</h2>
             <div className="flex flex-wrap gap-x-6 gap-y-6 mx-auto justify-center">
                 {trendingMovies?.map((movie) => {
+                    const isAlreadyPresent = checkIfPresentInWatchlist(movie.id);
                     return (
                         <div
                             key={movie.id}
-                            className="rounded-xl cursor-pointer w-1/5 transition-all duration-300 hover:scale-105"
+                            className="rounded-xl w-1/5 transition-all duration-300"
                             style={{
                                 boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
                             }}>
@@ -50,8 +68,18 @@ const MovieList = () => {
                                         <span key={id + idx}>{getGenreName(id, genres)}{idx !== movie.genre_ids.length - 1 ? " • " : ""}</span>
                                     )
                                 })}
-                                <p><i>Language:</i> {movie.original_language}</p>
                                 <p><i>Release Date:</i> {movie.release_date}</p>
+
+                                {!isAlreadyPresent ? <button
+                                    onClick={() => handleAddToWatchList(movie)}
+                                    className="flex justify-center items-center gap-x-2 cursor-pointer w-full p-2 border-gray-400 rounded-4xl mt-4 mb-2 bg-gray-300 hover:bg-yellow-600/40">
+                                    <Plus />
+                                    Watchlist
+                                </button> : <button
+                                    onClick={() => handleRemoveToWatchList(movie.id)}
+                                    className="flex justify-center items-center gap-x-2 cursor-pointer w-full p-2 border-gray-400 rounded-4xl mt-4 mb-2 bg-red-300 hover:bg-red-600/40">
+                                    Remove from Watchlist
+                                </button>}
                             </div>
                         </div>
                     )
